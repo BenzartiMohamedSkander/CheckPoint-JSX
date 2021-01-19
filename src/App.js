10814c4438ecd3319a48eb53f2d764acfec3c37e
unknown <benzarti.skander@gmail.com>
2021-01-19 11:44:46 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import imageInSrc from './image-src.jpg' 
 import './App.css';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+    <div style={{border:"solid 1px black", maxWidth:"100vw"}}>
+
+      <h1 className="title red">Skander Benzarti</h1>
+
+      <br />
+
+        <img src={imageInSrc } width="100%"/>
+
+          <br />
+
+            <img src="/profil.jpg" />
+
+</div>
+
+   
+<video width="320" height="240" controls >
+<source src="/Videos/Beach.mp4" type="video/mp4"/>
+</video>
+
+
+</div> 
   );
 }
 
